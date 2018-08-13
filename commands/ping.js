f6833f1a176f191a9fa0a98e4bd1bd3d78d031cf
zaozaostan <zaozaostan@gmail.com>
2018-08-13 11:48:44 -0700

    Change to anidiotsguide command handler

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,3 @@
-module.exports = {
-    name: 'ping',
-    description: 'Ping!',
-    execute(message, args) {
-        message.channel.send('Pong.');
-    },
-};
+exports.run = (client, message, args) => {
+    message.channel.send("ponasdflkjaslkdfjalksdjfadsklg!").catch(console.error);
+}
--- index.js
+++ index.js
@@ -1,38 +1,28 @@
-const fs = require('fs');
 const Discord = require('discord.js');
 const {prefix, discordToken} = require('./config.json');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    // set a new item in the Collection
-    // with the key as the command name and the value as the exported module
-    client.commands.set(command.name, command);
-}
-
 client.on('ready', () => {
   console.log('Ready!');
 });
 
-client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+client.on("message", message => {
+  if (message.author.bot) return;
+  if(message.content.indexOf(prefix) !== 0) return;
 
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const command = args.shift().toLowerCase();
+  // This is the best way to define args. Trust me.
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
 
-	if (!client.commands.has(command)) return;
-
-	try {
-		client.commands.get(command).execute(message, args);
-	}
-	catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
+  // The list of if/else is replaced with those simple 2 lines:
+  try {
+    let commandFile = require(`./commands/${command}.js`);
+    commandFile.run(client, message, args);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 client.login(discordToken);
