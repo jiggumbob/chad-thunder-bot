37de892fe4fd4f4973adf520ef56b937a3320d20
jiggumbob <34716142+jiggumbob@users.noreply.github.com>
2018-08-17 17:12:51 -0700

    Fixed indenting

--- commands/clear.js
+++ commands/clear.js
@@ -1,17 +1,15 @@
 exports.run = (client, message, args) => {
-  // Check if the user has enough permissions
-  if (!message.channel.permissionsFor(message.member).has("MANAGE_MESSAGES")) {
-    message.channel.sendMessage("Sorry, you don't have the permission to execute the command \""+message.content+"\"");
-    return;
-  }
-  
-  //checks if there is a number after
-  if (isNaN(args[0])) {
-    message.channel.send("Use a number as an argument.");
-    return;
-  }
-    
-  message.channel.bulkDelete(+args[0] + 1); // one extra to delete the user's message
+    // Check if the user has enough permissions
+    if (!message.channel.permissionsFor(message.member).has("MANAGE_MESSAGES")) {
+        message.channel.sendMessage("Sorry, you don't have the permission to execute the command \"" + message.content + "\"");
+        return;
+    }
+
+    //checks if there is a number after
+    if (isNaN(args[0])) {
+        message.channel.send("Use a number as an argument.");
+        return;
+    }
+
+    message.channel.bulkDelete(+args[0] + 1); // one extra to delete the user's message
 }
-
-
--- commands/ping.js
+++ commands/ping.js
@@ -1,31 +1,29 @@
 exports.run = async (client, message, args) => {
-  // calculates the time difference between when user's message was sent and when the bot sent one back
+    // calculates the time difference between when user's message was sent and when the bot sent one back
 
-  let responseMessage =  await message.channel.send("Pinging...");
-  let latency = responseMessage.createdTimestamp - message.createdTimestamp;
-  
-  // delete first ping message, resend new one, and recalculate (sometimes negative latency error happens)
-  while (latency < 0) {
-    responseMessage.delete();
-    responseMessage = await message.channel.send("Pinging again...");
-    latency = responseMessage.createdTimestamp - message.createdTimestamp;
-  }
-  
-  await responseMessage.edit({
-    embed: {
-      color: 0xffff00,
-      
-      title: "Chad ThunderBot",
-      
-      fields : [
-        {
-          name:'Ping',
-          
-          value: latency + " ms",
-        } 
-      ]
+    let responseMessage = await message.channel.send("Pinging...");
+    let latency = responseMessage.createdTimestamp - message.createdTimestamp;
+
+    // delete first ping message, resend new one, and recalculate (sometimes negative latency error happens)
+    while (latency < 0) {
+        responseMessage.delete();
+        responseMessage = await message.channel.send("Pinging again...");
+        latency = responseMessage.createdTimestamp - message.createdTimestamp;
     }
-  });
-  
-  
+
+    await responseMessage.edit({
+        embed: {
+            color: 0xffff00,
+
+            title: "Chad ThunderBot",
+
+            fields: [{
+                name: 'Ping',
+
+                value: latency + " ms",
+            }]
+        }
+    });
+
+
 }
