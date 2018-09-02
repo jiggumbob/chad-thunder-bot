f9dce02e2c3bdcab958684e518315069f19b4c28
Glitch (chad-thunder-beta) <none>
2018-09-02 04:54:24 +0000

    Fixed bug in register command

--- util/economy/register.js
+++ util/economy/register.js
@@ -2,25 +2,18 @@ var sql_connection = require("../sql-connection.js").sql_connection;
 
 exports.registerUser = async function registerUser (context) {
     let user_id = context.author.id;
-    sql_connection.connect();
   
-    // 1. check if user is already registered
-    let exists_query = "SELECT TOP 1 1 FROM UserInfo WHERE id = " + user_id + ";";
-    let result;
-    await sql_connection.query(exists_query, function(error, results, fields) {
-        result = results;
+    // check if user is already registered
+    sql_connection.query("SELECT 1 FROM UserInfo WHERE user_id = " + user_id, function(error, results, fields) {
+        if(results.length > 0) {
+            context.channel.send("You are already registered, no need to register again!");
+            return;
+        } else {
+            // not registered, register them with 100 chad bucks to start
+            let add_query = "INSERT INTO UserInfo (user_id, chad_bucks) VALUES (" + user_id + ", 100)";
+            sql_connection.query(add_query, function(error, results, fields) {
+                context.channel.send("You are now registered to the Chad Economy and have been given 100 Chad Bucks! Have fun!");
+            });
+        }
     });
-  
-    if(typeof result === "undefined") {
-        context.channel.send("You are already registered, no need to register again!");
-        return;
-    }
-  
-    // 2. register user and give them 100 chad bucks to start with
-    let add_query = "INSERT INTO UserInfo (user_id, chad_bucks) VALUES (" + user_id + ", 100);"
-    await sql_connection.query(add_query, function(error, results, fields) {
-        context.channel.send("You are now registered to the Chad Economy and have been given 100 Chad Bucks! Have fun!");
-    });
-
-    sql_connection.end();
 }
