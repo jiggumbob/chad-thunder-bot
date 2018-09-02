f29f6cb90029ea8d0207988a5cd4cffddb7acbf2
Glitch (chad-thunder-beta) <none>
2018-09-02 23:59:40 +0000

    Added embed "no result found" messages to google and wikipedia commands, and embed error message to invalid reddit subcommand

--- commands/google.js
+++ commands/google.js
@@ -1,5 +1,6 @@
 const {google} = require('googleapis');
 const customsearch = google.customsearch('v1');
+const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
     var cutArgs = message.content.substr(message.content.indexOf(" ") + 1);
@@ -18,8 +19,18 @@ exports.run = async (client, message, args) => {
         num: 1,
         safe: active
     });
-
-    message.channel.send(JSON.parse(JSON.stringify(res.data.items['0'].link)));
+    
+    try {
+        message.channel.send(JSON.parse(JSON.stringify(res.data.items['0'].link)));
+    } catch(e) {
+        // there was no result
+        let embed = new Discord.RichEmbed();
+        embed.setTitle("No Result Found");
+        embed.setDescription("Uh oh! No result was found for that search!");
+        embed.setThumbnail("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/147/confused-face_1f615.png");
+        embed.setColor(0xFF524C);
+        message.channel.send(embed);
+    }
 }
 
 exports.conf = {
