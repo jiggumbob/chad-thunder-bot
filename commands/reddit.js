e5463b15d4f8fa04b83e7ea1fd6fdf695018aed0
Glitch (chad-thunder-beta) <none>
2018-09-09 19:18:29 +0000

    Fixed comment and removed import in commands/reddit.js

--- commands/reddit.js
+++ commands/reddit.js
@@ -1,9 +1,8 @@
 /**
- * Provides a command to create embed messages in a streamlined manner.
+ * Defines the reddit command.
  *
- * Ensures consistency in the design color throughout the bot messages and allows for
- * easy switching between different colors without going through tons of files. Also provides a 
- * list of emojis for use by embed messages, eliminating the need to put huge links in many files.
+ * Distinguishes between the two subcommands of the reddit command, 
+ * then uses the reddit-util file to handle each of the subcommands.
  *
  * @author Jude Markabawi, Stanley Wang.
  * @license See the LICENSE file for details.
@@ -11,12 +10,6 @@
 
 const snoowrap = require("snoowrap");
 const redditutil = require("../util/reddit/reddit-util.js");
-const Reddit = new snoowrap({
-    userAgent: "Discord: chad-thunder-bot",
-    clientId: process.env.REDDIT_CLIENT_ID,
-    clientSecret: process.env.REDDIT_CLIENT_SECRET,
-    refreshToken: process.env.REDDIT_REFRESH_TOKEN
-});
 const embedTool = require("../util/embed-message-tool.js");
 
 exports.run = async (client, message, args) => {
