3864bfc30312abda6975d2362412bd1de9a44bce
Glitch (chad-thunder-beta) <none>
2018-09-10 02:35:56 +0000

    Added some roulette files (bet class)

---
+++ util/games/roulette/bet-groups.js
@@ -0,0 +1,37 @@
+/**
+ * Defines groups of numbers that fit certain roulette bet groups.
+ *
+ * Every group contains certain numbers that fulfill its condition. These are used 
+ * to find which bets are successful for certain numbers and which aren't.
+ *
+ * @author Jude Markabawi, Stanley Wang.
+ * @license See the LICENSE file for details.
+ */
+
+const groupCollections = [odd, even, red, black, one_to_18, eighteen_to_36];
+
+const odd = {
+    name: "odd",
+    values: [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35]
+};
+const even = {
+    name: "even",
+    values: [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36]
+};
+const red = {
+    name: "red",
+    values: [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+};
+const black = {
+    name: "black",
+    values: [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+};
+const one_to_18 = {
+    name: "1_to_18",
+    values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+};
+const eighteen_to_36 = {
+    name: "18_to_36",
+    values: [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
+};
+
