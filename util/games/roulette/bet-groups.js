4e5a7307a3610ce6a62522c29b66c34e2606269d
Glitch (chad-thunder-beta) <none>
2018-09-10 02:50:17 +0000

    Fix name of 19-36 betting group

--- util/games/roulette/bet-groups.js
+++ util/games/roulette/bet-groups.js
@@ -8,7 +8,7 @@
  * @license See the LICENSE file for details.
  */
 
-const groupCollections = [odd, even, red, black, one_to_18, eighteen_to_36];
+const groupCollections = [odd, even, red, black, one_to_18, nineteen_to_36];
 
 const odd = {
     name: "odd",
@@ -30,8 +30,8 @@ const one_to_18 = {
     name: "1_to_18",
     values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
 };
-const eighteen_to_36 = {
-    name: "18_to_36",
+const nineteen_to_36 = {
+    name: "19_to_36",
     values: [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
 };
 
