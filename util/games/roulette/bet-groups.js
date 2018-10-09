2d9c726cf0eadab7c1806f0aecaed58d9b757af1
Glitch (chad-thunder-beta) <none>
2018-10-09 00:06:25 +0000

    Rename column and dozen betting groups

--- util/games/roulette/bet-groups.js
+++ util/games/roulette/bet-groups.js
@@ -39,32 +39,32 @@ const nineteen_to_36 = {
     values: [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
 };
 const first_12 = {
-    name: "first12",
+    name: "1st12",
     multiplier: 2,
     values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 };
 const second_12 = {
-    name: "second12",
+    name: "2nd12",
     multiplier: 2,
     values: [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
 };
 const third_12 = {
-    name: "third12",
+    name: "3rd12",
     multiplier: 2,
     values: [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
 };
 const first_column = {
-    name: "firstcolumn",
+    name: "1stcolumn",
     multiplier: 2,
     values: [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34]
 };
 const second_column = {
-    name: "secondcolumn",
+    name: "2ndcolumn",
     multiplier: 2,
     values: [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35]
 };
 const third_column = {
-    name: "thirdcolumn",
+    name: "3rdcolumn",
     multiplier: 2,
     values: [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
 };
